0f05d69ff71823af7cd862a4946d5d2d607c0031
8i-ght <113914772+8i-ght@users.noreply.github.com>
2023-02-16 23:57:42 -0600

    finish if statement

---
+++ review/lht_table.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: Kayden Roberts
+   Date:   2/13/2023
+
+	
+*/
+
+var thisDay = new Date("August 30, 2018");
+
+var tableHTML = "<table id='eventTable'>";
+tableHTML += "<caption> Upcoming Events </caption>";
+tableHTML += "<tr><th>Date</th><th>Event</th><th>Price</th></tr>";
+     
+var endDate = new Date(thisDay.getTime() + 14 * 24 * 60 * 60 * 1000);
+
+for (var i = 0; i < eventDates.length; i++) {
+   var eventDate = new Date(eventDates[i]);
+   var eventDay = toDateString("eventDate");
+
+}
+
